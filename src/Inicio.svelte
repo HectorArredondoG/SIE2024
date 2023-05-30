63c6311b112338d77c7708c3b121f52104264e6b
hectorArredondo34 <113058902+hectorArredondo34@users.noreply.github.com>
2023-05-30 12:51:23 -0600

    Update

--- src/Inicio.svelte
+++ src/Inicio.svelte
@@ -6,37 +6,12 @@
 
 <main>
 
-    <div class="container menu">
-        <div class="row">
-            <div class="col">
-                <div class="card" style="width: 22rem;">
-                    <a href="/Inscripcion" use:link><img src="./img/insc.png" class="card-img-top" alt="..."></a>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card" style="width: 22rem;">
-                    <a href="/Reinscripcion" use:link><img src="./img/rcn.png" class="card-img-top" alt="..."></a>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card" style="width: 22rem;">
-                    <a href="/Tramites" use:link><img src="./img/tra.png" class="card-img-top" alt="..."></a>
-                </div>
-            </div>
-        </div>
-    </div>
-
+    
 
 </main>
 
 <style>
 
-    img {
-        height: 273px;
-    }
-
-    .menu {
-        margin-top: 100px;
-    }
+   
 
 </style>
