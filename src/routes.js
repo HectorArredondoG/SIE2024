06e216e68ea496760674ef1590ef58ce5fbcbbe2
hectorArredondo34 <113058902+hectorArredondo34@users.noreply.github.com>
2023-05-30 14:45:39 -0600

    update

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,21 @@
 import Inicio from './Inicio.svelte'
+// Asuntos Escolares
+import Inicio_Asuntos from './AE/Inicio_Asuntos.svelte'
 import Inscripcion from './AE/Inscripcion.svelte'
 import Reinscripcion from './AE/Reinscripcion.svelte'
 import Tramites from './AE/Tramites.svelte'
+// Profesores
 import ProfTitular from './Prof/ProfTitular.svelte'
 
 
 const routes = {
     '/' : Inicio,
+    // Asuntos Escolares
+    '/Inicio_Asuntos' : Inicio_Asuntos,
     '/Inscripcion' : Inscripcion,
     '/Reinscripcion' : Reinscripcion,
     '/Tramites' : Tramites,
+    //Profesores
     '/ProfTitular': ProfTitular,
 }
 
