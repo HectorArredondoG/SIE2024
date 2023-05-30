16d36ec0e527be49c94e79da0ee9e21248cc112c
JuanCarlosOlmos <126510195+JuanCarlosOlmos@users.noreply.github.com>
2023-05-30 12:35:42 -0600

    Juan

--- src/App.svelte
+++ src/App.svelte
@@ -2,14 +2,16 @@
 
 	import Router from 'svelte-spa-router'
 	import routes from './routes'
-
+	import Footer from './Componentes/Footer.svelte';
+	import Header from './Componentes/Header.svelte';
 	import { link } from 'svelte-spa-router'
  
 </script>
 
 <main>
-
+	<Header/>
 	<Router { routes }/>
+	<Footer />
 	
 </main>
 
