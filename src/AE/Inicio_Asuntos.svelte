d920fe057af7340556d6535abcc5a1075cb7bb74
hectorArredondo34 <113058902+hectorArredondo34@users.noreply.github.com>
2023-05-31 10:11:06 -0600

    Update

--- src/AE/Inicio_Asuntos.svelte
+++ src/AE/Inicio_Asuntos.svelte
@@ -7,13 +7,8 @@
     <div class="row">
         <div class="col">
             <div class="card" style="width: 18rem;">
-                <img src="..." class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">Card title</h5>
-                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
+                <a href="/Inscripcion"> <img src="./img/Diseño/insc.png" alt=""> </a>
+            </div>
         </div>
     </div>
 
